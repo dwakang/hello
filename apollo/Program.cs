f1fff72b60b7f139852b1a7818e72d9c10b18530
sun@a.com <sun>
2019-01-21 13:04:10 +0900

    monday 21

--- apollo/Program.cs
+++ apollo/Program.cs
@@ -11,6 +11,7 @@ namespace apollo
         static void Main(string[] args)
         {
             Console.WriteLine("Hi! GitHub Enterprise");
+            Console.WriteLine("Good Mornig Monday");
         }
     }
 }
